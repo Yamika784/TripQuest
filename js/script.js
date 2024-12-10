d4676a09619207b6f1cd653ec606b40b3216e16d
Fujita <24aw0133@jec.ac.jp>
2024-12-10 23:23:22 +0900

    1210

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 let btn = document.querySelector(".header__btn");
 let lines = document.querySelectorAll(".line");
 let nav = document.querySelector("nav");
+// let navLinks = document.querySelectorAll("nav ul li a");
+
 
 btn.addEventListener("click", function() {
     btn.classList.toggle("open");
@@ -11,3 +13,20 @@ btn.addEventListener("click", function() {
     nav.classList.toggle("open");
 
 })
+
+// // / ナビゲーションリンクをクリックしたとき、メニューを閉じる
+// navLinks.forEach(link => {
+//     link.addEventListener("click", function () {
+//         btn.classList.remove("open");
+//         lines.forEach(line => line.classList.remove("open"));
+//         nav.classList.remove("open");
+//     });
+// });
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+  });
+  
